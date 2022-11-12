e9d3146b0a6433915dfaba18f78d5abac57370ef
Muhammad Afif Abroory <afif.abroory@gmail.com>
2022-11-12 18:01:08 +0800

    Add: AppBar components for desktop

---
+++ src/components/AppBar/index.js
@@ -0,0 +1,23 @@
+import { AppBar, Toolbar, IconButton, Box } from '@mui/material';
+import { Logout } from '@mui/icons-material';
+
+import TaskManagementLogoLight from '../../assets/Task Management Logo - Light.png';
+
+export default () => {
+  return (
+    <AppBar position='static' sx={{ py: 1 }}>
+      <Toolbar variant='regular'>
+        <Box sx={{ width: 1, display: 'flex', justifyContent: 'center' }}>
+          <Box
+            component='img'
+            src={TaskManagementLogoLight}
+            sx={{ height: 50 }}
+          ></Box>
+        </Box>
+        <IconButton edge='start' color='inherit' aria-label='menu'>
+          <Logout fontSize='large' />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
