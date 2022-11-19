005d59aafd87967776fd21975fa1ffa7e5f8f6d7
Muhammad Afif Abroory <afif.abroory@gmail.com>
2022-11-19 20:14:12 +0800

    Update: AppBar margin left

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -5,19 +5,33 @@ import TaskManagementLogoLight from '../assets/Task Management Logo - Light.png'
 
 export default () => {
   return (
-    <AppBar position='static' sx={{ py: 1 }}>
-      <Toolbar variant='regular'>
-        <Box sx={{ width: 1, display: 'flex', justifyContent: 'center' }}>
+    <Box
+      component='div'
+      position='relative'
+      sx={{
+        ml: { xs: '0', sm: '260px' },
+      }}
+    >
+      <AppBar position='absolute' sx={{ py: 1 }}>
+        <Toolbar variant='regular'>
           <Box
-            component='img'
-            src={TaskManagementLogoLight}
-            sx={{ height: 50 }}
-          />
-        </Box>
-        <IconButton edge='start' color='inherit' aria-label='menu'>
-          <Logout fontSize='large' />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+            sx={{
+              width: 1,
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Box
+              component='img'
+              src={TaskManagementLogoLight}
+              sx={{ height: 50 }}
+            />
+          </Box>
+          <IconButton edge='start' color='inherit' aria-label='menu'>
+            <Logout fontSize='large' />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
