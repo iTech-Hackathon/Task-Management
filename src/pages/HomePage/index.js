aa1fac7d2418b4685d8dd253187f00fb37e51735
Bhakti Ramadhani <bakti0048@gmail.com>
2022-11-25 23:33:54 +0800

    nambahin dikit

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,6 +2,69 @@ import { AppBar, SideBar, Tasks } from '../../components';
 import { Box } from '@mui/material';
 
 export default () => {
+  // data sementara aja dulu
+  const tasks = [
+    {
+      id: '1',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: true,
+    },
+    {
+      id: '2',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: true,
+    },
+    {
+      id: '3',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: true,
+    },
+    {
+      id: '4',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: true,
+    },
+    {
+      id: '5',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: true,
+    },
+    {
+      id: '6',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: false,
+    },
+    {
+      id: '7',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: false,
+    },
+    {
+      id: '8',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: false,
+    },
+    {
+      id: '9',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: false,
+    },
+    {
+      id: '10',
+      judul: 'Membuat halaman landing',
+      date: '#202203-01-02',
+      isCompleted: false,
+    },
+  ];
   return (
     <Box>
       <SideBar />
@@ -24,12 +87,18 @@ export default () => {
             sx={{
               p: 3,
               display: 'flex',
+              flexDirection: {
+                lg: 'row',
+                md: 'row',
+                sm: 'column',
+                xs: 'column',
+              },
               justifyContent: 'center',
               gap: '24px',
             }}
           >
-            <Tasks />
-            <Tasks />
+            <Tasks heading='Active Tasks' tasks={tasks} />
+            <Tasks heading='Completed Tasks' tasks={tasks} />
           </Box>
         </Box>
       </Box>
