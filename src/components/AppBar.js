45263da7f84f0308b0a168ba56755778d2107e5c
Daffa Ilhami <mdaffailhami@gmail.com>
2022-12-03 15:19:10 +0800

    Add: Sign Out Dialog

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,7 +1,9 @@
-import { AppBar, Toolbar, IconButton, Box } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Box, Button } from '@mui/material';
 import { Logout } from '@mui/icons-material';
 
 import TaskManagementLogoLight from '../assets/Task Management Logo - Light.png';
+import { AlertDialog } from '.';
+import { useState } from 'react';
 
 export default () => {
   return (
@@ -20,10 +22,41 @@ export default () => {
             sx={{ height: 50 }}
           />
         </Box>
-        <IconButton edge='start' color='inherit' aria-label='menu'>
-          <Logout fontSize='large' />
-        </IconButton>
+        <SignOutButton />
       </Toolbar>
     </AppBar>
+  );
+};
+
+const SignOutButton = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const showDialog = () => setIsDialogOpen(true);
+
+  const closeDialog = () => setIsDialogOpen(false);
+
+  return (
+    <>
+      <IconButton
+        edge='start'
+        color='inherit'
+        aria-label='menu'
+        onClick={showDialog}
+      >
+        <Logout fontSize='large' />
+      </IconButton>
+      <AlertDialog
+        isOpen={isDialogOpen}
+        title='Sign Out?'
+        content='Are you sure you want sign out?'
+        actions={
+          <>
+            <Button onClick={closeDialog}>CANCEL</Button>
+            <Button onClick={() => {}}>OK</Button>
+          </>
+        }
+        onOutsideOfTheDialogClicked={closeDialog}
+      />
+    </>
   );
 };
