cb838e3885253a44af41a683e29e90a3963009b9
Daffa Ilhami <mdaffailhami@gmail.com>
2022-12-03 15:18:18 +0800

    Import Alert Dialog

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import AppBar from './AppBar';
 import SideBar from './SideBar';
 import Tasks from './Tasks';
+import AlertDialog from './AlertDialog';
 
-export { AppBar, SideBar, Tasks };
+export { AppBar, SideBar, Tasks, AlertDialog };
